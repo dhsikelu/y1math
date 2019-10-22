f3c35624184ae50c4774a5b913f93f52a5464d82
dhsikelu <38091580+dhsikelu@users.noreply.github.com>
2019-10-22 14:28:10 +0800

    Update t07numberpatterns_powers2.py

--- t07numberpatterns_powers2.py
+++ t07numberpatterns_powers2.py
@@ -1,8 +1,5 @@
 # Generate powers of 2 from 1 to 10
 
-START = 1
-END = 10
-num = 2
-
-for i in range(START, END+1):
-    print(num ** i, end=' ')
+for i in range(1, 11):
+    i = 2 ** i
+    print(i)
